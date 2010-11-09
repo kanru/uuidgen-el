d1d5afa498916907db2b4bae6ec9e70be75d6789
Kan-Ru Chen <kanru@kanru.info>
2010-11-09 11:34:09 +0800

    Add clone url

--- uuid.el
+++ uuid.el
@@ -28,6 +28,10 @@
 ;; v3, v4 and v5 generation.  The resolution of the time based UUID is
 ;; microseconds, which is 10 times of the suggested 100-nanosecond
 ;; resolution, but should be enough for general usage.
+;;
+;; Get development version from git:
+;;
+;;     git clone git://github.com/kanru/uuid-el.git
 
 ;;; TODO:
 ;;
